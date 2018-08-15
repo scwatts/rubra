31f5d615ced6a33390a8088509290793d133bacf
scwatts <s.watts2@student.unimelb.edu.au>
2018-08-15 15:06:30 +1000

    Update version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='Rubra',
-    version='0.1.5',
+    version='0.1.5@scwatts.0.1',
     author='Bernie Pope',
     author_email='bjpope@unimelb.edu.au',
     packages=['rubra'],
